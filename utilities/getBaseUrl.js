a36698381614170eb7cdbc53fdf9e3e0182f53b0
Kevin Garry <kevingarry97@gmail.com>
2023-09-05 14:58:54 +0200

    URL fix

--- utilities/getBaseUrl.js
+++ utilities/getBaseUrl.js
@@ -6,7 +6,7 @@ const { hostApi, appEnv } = publicRuntimeConfig;
 export default function GetBaseUrl(host) {
   let api = `${hostApi}/v1`;
   if (appEnv !== 'development') {
-    api = host ? `https://${host}/api/v1` : 'http://localhost:8081/api/v1';
+    api = host ? `https://${host}/api/v1` : '/api/v1';
   }
   return api;
 }
